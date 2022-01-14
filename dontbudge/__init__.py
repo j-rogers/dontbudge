f48c53d82d8f78672218d7af27458fcc17678e38
Josh <joshua.earl.rogers@gmail.com>
2022-01-14 02:23:27 +0000

    debug mode can be set via environment variable

--- dontbudge/__init__.py
+++ dontbudge/__init__.py
@@ -6,10 +6,11 @@ from dontbudge.dashboard import dashboard
 from dontbudge import database
 
 SECRET = environ.get('FLASK_SECRET_KEY')
+DEBUG = environ.get('DONTBUDGE_DEBUG')
 
 def create_app():
     app = Flask(__name__)
-    #app.config['DEBUG'] = True
+    app.config['DEBUG'] = True if DEBUG else False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db/dontbudge.sqlite3'
     app.config['SECRET_KEY'] = SECRET
     app.register_blueprint(api)
